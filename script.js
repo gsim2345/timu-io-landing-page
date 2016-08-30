36c7cfbd04edc0cffef9b93420a4f7ef86f76a8d
Zoltan Simon <zoltan@zoltansimon.me>
2016-08-30 21:24:23 +0200

    Code Review and Fixes

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
-
-//Initializing material design
 $( document ).ready(function() {
+  //Initializing material design
   $.material.init();
 
-
-// sets the page size to the height of the window
+  // sets the page size to the height of the window
   $(".pageContainer").css("min-height", $(window).height());
 
-//Click event handler for nav-items
-  $('.navbar-item').on('click',function(){
-  //Remove any previous active classes
-    $('.navbar-item').removeClass('active');
-  //Add active class to the clicked item
-    $(this).addClass('active');
-});
-
-//Scrollspy
+  //Scrollspy
   $('body').scrollspy({ target: '.navbar' });
 
-//Waypoints
-  $("#about").waypoint(function(direction) {
-    if (direction == "down") {
-       var x = document.getElementsByClassName("changeColor");
-       var i;
-       for (i = 0; i < x.length; i++) {
-            x[i].style.color = "black";
-          }
-     }
-    if (direction == "up") {
-       var x = document.getElementsByClassName("changeColor");
-       var i;
-       for (i = 0; i < x.length; i++) {
-             x[i].style.color = "white";
-         }
-      }
+  //Waypoints
+  $('#about').waypoint(function(direction) {
+    if (direction == 'down') {
+      $('.navbar-nav > li').css('color','gray');
+    }
+    if (direction == 'up') {
+      $('.navbar-nav > li').css('color','white');
+    }
   }, {offset: 25});
 
-  });
+});
