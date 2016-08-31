75d01c9d395f8ade62df5a476b13cb4aec1466ac
Zoltan Simon <zoltan@zoltansimon.me>
2016-08-31 09:13:18 +0200

    add jquery-hashchange and thx message

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ $( document ).ready(function() {
   //Initializing material design
   $.material.init();
 
+  $(window).hashchange( function(){
+    if (location.hash == "#thx") {
+      $('#thx-msg').show();
+      $('#mc-embedded-subscribe-form').hide();
+    } else {
+      $('#thx-msg').hide();
+      $('#mc-embedded-subscribe-form').show();
+    }
+  })
+  $(window).hashchange();
   // sets the page size to the height of the window
   $(".pageContainer").css("min-height", $(window).height());
 
