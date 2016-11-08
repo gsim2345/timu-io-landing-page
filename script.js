6fa38dae774722b3840a2e39a5846070440de53c
Gabriella Simon <gb@gabriellasimon.me>
2016-11-08 10:13:14 +0100

    Beautified

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-$( document ).ready(function() {
-  //Initializing material design
-  $.material.init();
+$(document).ready(function() {
+    //Initializing material design
+    $.material.init();
 
-  // sets the page size to the height of the window
-  $(".pageContainer").css("min-height", $(window).height());
+    // sets the page size to the height of the window
+    $(".pageContainer").css("min-height", $(window).height());
 
-  //Scrollspy
-  $('body').scrollspy({ target: '.navbar' });
+    //Scrollspy
+    $('body').scrollspy({
+        target: '.navbar'
+    });
 
-  //Waypoints
-  $('#about').waypoint(function(direction) {
-    if (direction == 'down') {
-      $('.navbar-nav > li').css('color','gray');
-    }
-    if (direction == 'up') {
-      $('.navbar-nav > li').css('color','white');
-    }
-  }, {offset: 25});
+    //Waypoints
+    $('#about').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('.navbar-nav > li').css('color', 'gray');
+        }
+        if (direction == 'up') {
+            $('.navbar-nav > li').css('color', 'white');
+        }
+    }, {
+        offset: 25
+    });
 
 });
